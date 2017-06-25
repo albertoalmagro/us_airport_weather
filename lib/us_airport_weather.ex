35f5ac794914d9d1c2b9cafa087b060164c617b8
Alberto Almagro <alberto.almagro@rakuten.com>
2017-06-25 17:42:30 +0200

    Initial commit

---
+++ lib/us_airport_weather.ex
@@ -0,0 +1,18 @@
+defmodule UsAirportWeather do
+  @moduledoc """
+  Documentation for UsAirportWeather.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UsAirportWeather.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/us_airport_weather_test.exs
@@ -0,0 +1,8 @@
+defmodule UsAirportWeatherTest do
+  use ExUnit.Case
+  doctest UsAirportWeather
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
